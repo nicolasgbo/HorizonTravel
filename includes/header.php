ca1120f9d1b2666fe3ec781199322f143d0bec5c
Nicolas <nicolassg121@gmail.com>
2025-07-08 11:53:53 -0300

    Subindo a logo do navegador!

--- includes/header.php
+++ includes/header.php
@@ -36,6 +36,7 @@ if (session_status() === PHP_SESSION_NONE) {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"
         integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="icon" href="horizontravel.ico" type="image/x-icon">
 </head>
 
 <!-- Script para máscara do telefone -->
